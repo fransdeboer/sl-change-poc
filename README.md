064727f04592c9f6cdef437b0cc94297bcd3bf7b
Frans de Boer <frans.deboer@iddinkgroup.com>
2024-07-26 14:31:12 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,5 @@
+clone this repository
+
+run `npm ci` to install all dependencies
+
+run `npm run dev` to start the application. Open a browser at http://localhost:5173/ 
---
+++ src/my-element.ts
@@ -0,0 +1,69 @@
+import type { ScopedElementsMap } from '@open-wc/scoped-elements/lit-element.js';
+import { ScopedElementsMixin } from '@open-wc/scoped-elements/lit-element.js';
+import { Checkbox, CheckboxGroup } from '@sl-design-system/checkbox';
+import { Form, FormField } from '@sl-design-system/form';
+import { Radio, RadioGroup } from '@sl-design-system/radio-group';
+import { LitElement, css, html } from 'lit';
+import { customElement, state } from 'lit/decorators.js';
+
+/**
+ * An example element.
+ */
+@customElement('my-element')
+export class MyElement extends ScopedElementsMixin(LitElement) {
+  static get scopedElements(): ScopedElementsMap {
+    return {
+      'sl-form': Form,
+      'sl-form-field': FormField,
+      'sl-radio': Radio,
+      'sl-radio-group': RadioGroup,
+      'sl-checkbox': Checkbox,
+      'sl-checkbox-group': CheckboxGroup
+    };
+  }
+
+  @state() mayChangeColumnProperties = false;
+
+  private visibilityOptions: string[] = [];
+
+  render() {
+    return html`
+      <sl-form>
+        <sl-form-field label="Who is allowed to see this column and the results in it?">
+            <sl-checkbox-group
+              name="visibility"
+              .value=${this.visibilityOptions}
+              @sl-change=${(event: CustomEvent<string[]>) => {
+                console.log(event.detail);
+              }}
+            >
+              <sl-checkbox value="isVisibleForStudent">Parents and students</sl-checkbox>
+              <sl-checkbox value="isVisibleForTeacher">Teaching staff (OP)</sl-checkbox>
+            </sl-checkbox-group>
+          </sl-form-field>
+
+
+          <sl-form-field label=${'Can an OP-er change column properties?'}>
+          <sl-radio-group
+            name="change-column-properties"
+            .value=${this.mayChangeColumnProperties}
+            @sl-change=${(event: CustomEvent<boolean>) => (this.mayChangeColumnProperties = event.detail)}
+          >
+            <sl-radio .value=${false} checked>No</sl-radio>
+            <sl-radio .value=${true}>Yes</sl-radio>
+          </sl-radio-group>
+        </sl-form-field>
+
+      </sl-form>
+    `
+  }
+
+  static styles = css`
+  `
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'my-element': MyElement
+  }
+}
